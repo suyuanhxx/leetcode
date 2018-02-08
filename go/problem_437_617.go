e9a689c8153a0b80441982c1e07343ca9bd404c1
黄小旭 <huangxx@vstone.com.cn>
2018-02-08 15:04:09 +0800

    add leetcode problem

---
+++ go/problem_437_617.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+)
+
+// Definition for a binary tree node.
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func pathSum(root *TreeNode, sum int) int {
+	if root == nil {
+		return 0
+	}
+	pathSum(root, sum)
+	count := 0
+	num := root.Val
+	//cur := &root
+	if num == sum {
+		count++
+	}
+	if root.Left != nil {
+		root = root.Left
+		num += root.Val
+	}
+	if root.Right != nil {
+		root = root.Right
+		num += root.Val
+	}
+	return count
+}
+
+func mergeTrees(t1 *TreeNode, t2 *TreeNode) *TreeNode {
+	if t1 == nil {
+		return t2
+	}
+	if t2 == nil {
+		return t1
+	}
+	t1.Val += t2.Val
+	t1.Left = mergeTrees(t1.Left, t2.Left)
+	t1.Right = mergeTrees(t1.Right, t2.Right)
+	return t1
+}
+
+func main() {
+	left := TreeNode{1, nil, nil}
+	right := TreeNode{5, nil, nil}
+	left1 := TreeNode{-2, nil, nil}
+	right1 := TreeNode{4, &left, &right}
+	root := TreeNode{1, &left1, &right1}
+	//fmt.Println(pathSum(&root, 6))
+
+	left2 := TreeNode{-2, &left, &right}
+	root2 := TreeNode{1, &left2, &right1}
+
+	fmt.Println(mergeTrees(&root, &root2))
+
+}
---
+++ go/web.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/astaxie/beego"
+
+func main() {
+	 beego.Run("127.0.0.1:8089")
+}
